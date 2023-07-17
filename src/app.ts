056384aac848382bf28424516de14613ea518e58
sifat-fahimul <mdsifatfeni@gmail.com>
2023-07-18 01:53:14 +0600

    handle global error handler middlerware  done

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import routes from "./app/routes";
+
+import cookieParser from "cookie-parser";
+import { NotFoundHandler } from "./errors/NotFoundHandler";
+
+export const app: Application = express();
+//cors
+app.use(cors());
+//cookie
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//All Routes
+app.use("/api/v1", routes);
+
+//Global Error Handler
+app.use(globalErrorHandler);
+
+//handle not found
+app.use(NotFoundHandler.handle);
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { Error } from "mongoose";
+import config from "../../config";
+import ApiError from "../../errors/ApiError";
+import IGenericErrorMessage from "../../interface/error";
+import { handleCastError } from "../../errors/handleCastError";
+import handleValidationError from "../../errors/handleValidatoinError";
+
+const globalErrorHandler: ErrorRequestHandler = (
+  error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = "Something went wrong !";
+  let errorMessages: IGenericErrorMessage[] = [];
+
+  if (error?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error?.name === "CastError") {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode;
+    message = error.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: "",
+            message: error?.message,
+          },
+        ]
+      : [];
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== "production" ? error?.stack : undefined,
+  });
+};
+
+export default globalErrorHandler;
